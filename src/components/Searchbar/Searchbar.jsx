8a6be4a36e5ecd143752efdbdd19bdbac862150e
mswitas <mswitas@gmail.com>
2024-02-13 13:13:22 +0100

    fix import

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import PropTypes, { func } from "prop-types";
+import PropTypes from "prop-types";
 
 class Searchbar extends Component {
     static defaultProps = {
